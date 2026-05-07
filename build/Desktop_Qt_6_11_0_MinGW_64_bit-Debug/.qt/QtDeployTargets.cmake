70b2fba05b178d3bde4eb76a578b2ee3f8cfb023
lastcave <himalaa22006@gmail.com>
2026-05-07 22:02:27 +0300

    removed build

--- build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_untitled_FILE C:/Users/Owner/Desktop/Snake-Game/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/untitled.exe)
-set(__QT_DEPLOY_TARGET_untitled_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_untitled_RUNTIME_DLLS C:/Qt/6.11.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Multimedia.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Network.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Concurrent.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Core.dll)
