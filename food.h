819a7fecd48b9a8947fb83f226871b9a8e97ff93
Youssef Eladawy <yusufaladawy@gmail.com>
2026-05-12 05:53:49 +0300

    Finalizing

--- food.h
+++
@@ -1,7 +0,0 @@
-#ifndef FOOD_H
-#define FOOD_H
-
-// Food logic is handled directly via CellContent on the Board.
-// Kept as a placeholder so CMakeLists.txt compilation succeeds.
-
-#endif // FOOD_H
